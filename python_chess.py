2c95fefa85276bd196819c823c1203cea70943b6
Nicholas Glazkov <nic@glazkov.com>
2024-02-05 20:50:31 -0800

    Added move checking and updated formatting

--- python_chess.py
+++ python_chess.py
@@ -1,32 +1,47 @@
+"""
+import python-chess and stockfish to play chess
+"""
 import chess
 import chess.svg
 from stockfish import Stockfish
 
 stockfish = Stockfish(path="/usr/local/Cellar/stockfish/16/bin/stockfish")
-stockfish.set_depth(20) #How deep the AI looks
-stockfish.set_skill_level(20) #Highest rank stockfish
-stockfish.get_parameters() #Get all the parameters
+stockfish.set_depth(20)  # How deep the AI looks
+stockfish.set_skill_level(20)  # Highest rank stockfish
+stockfish.get_parameters()  # Get all the parameters
 
-board = chess.Board() #Create a new board
+board = chess.Board()  # Create a new board
+
 
 def display_board():
-    f = open("chess.svg", "w") # Open a file to write to
-    f.write(chess.svg.board(board)) # Write the board to the file
-    f.close() # Close the file
+    """
+    Display the chess board by writing it to a SVG file.
+    """
+    with open(
+        "chess.svg", "w", encoding="utf-8"
+    ) as f:  # Open a file to write to with explicit encoding
+        f.write(chess.svg.board(board))  # Write the board to the file
 
-display_board() #Display the board
+
+display_board()  # Display the board
 
 while not board.is_game_over():
-    inputmove = input("Input move (SAN format):") #Get the move from the user
-    board.push_san(inputmove) #Push the move to the board
+    inputmove = input("Input move (SAN format):")  # Get the move from the user
+    valid_move = chess.Move.from_uci(inputmove) in board.legal_moves
+    if valid_move is True:
+        board.push_san(inputmove)  # Push the move to the board
 
-    display_board() #Display the board
+        display_board()  # Display the board
 
-    stockfish.set_fen_position(board.fen()) #Set the position of the board
-    bestMove = stockfish.get_top_moves(1) #Get the best move
-    board.push_san(bestMove[0]['Move']) #Push the best move to the board
-    
-    display_board() #Display the board
+        stockfish.set_fen_position(board.fen())  # Set the position of the board
+        bestMove = stockfish.get_top_moves(1)  # Get the best move
+        print(
+            "Stockfish moves:", board.push_san(bestMove[0]["Move"])
+        )  # Push the best move to the board
+        display_board()  # Display the board
+
+    else:
+        print("Not a legal move, Please try again")
 
 
-print(board.outcome().winner)
+print(board.outcome())  # Print the winner of the game
