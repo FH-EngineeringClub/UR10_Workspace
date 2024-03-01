008e453e19c4e1883062dee3a03630d2f22c5b70
Nic Glazkov <nic@glazkov.com>
2024-02-29 21:42:38 -0800

    added comments

--- working_examples/coordinatespace.py
+++ working_examples/coordinatespace.py
@@ -8,11 +8,11 @@ rtde_io_ = rtde_io.RTDEIOInterface("192.168.56.101")
 rtde_receive_ = rtde_receive.RTDEReceiveInterface("192.168.56.101")
 control_interface = rtde_control.RTDEControlInterface("192.168.56.101")
 
-ANGLE = 45  # angle between the robot base and the chess board
-DX = -380  # Home TCP positon relative to base
+ANGLE = 45  # angle between the robot base and the chess board (in degrees)
+DX = -380  # Home TCP position relative to base (in mm)
 DY = -500
-BOARD_HEIGHT = 0.20  # height for the electromagnet to attach to pieces
-LIFT_HEIGHT = 0.40  # height of the lift
+BOARD_HEIGHT = 0.20  # height for the electromagnet to attach to pieces (in meters)
+LIFT_HEIGHT = 0.40  # height of the lift (in meters)
 
 
 def translate(x, y):
@@ -31,9 +31,9 @@ def move_to_square(pos, height):
             robot_position[0] / 1000,  # x
             robot_position[1] / 1000,  # y
             height,  # z (height of the chess board)
-            0.546,  # rx
-            3.062,  # ry
-            0.045,  # rz
+            0.546,  # rx (x rotation of TCP in radians)
+            3.062,  # ry (y rotation of TCP in radians)
+            0.045,  # rz (z rotation of TCP in radians)
         ],
         0.5,  # speed: speed of the tool [m/s]
         0.3,  # acceleration: acceleration of the tool [m/s^2]
@@ -47,9 +47,9 @@ def lift_piece(pos):
             robot_position[0] / 1000,  # x
             robot_position[1] / 1000,  # y
             LIFT_HEIGHT,  # z (height to lift piece)
-            0.546,  # rx
-            3.062,  # ry
-            0.045,  # rz
+            0.546,  # rx (x rotation of TCP in radians)
+            3.062,  # ry (y rotation of TCP in radians)
+            0.045,  # rz (z rotation of TCP in radians)
         ],
         0.5,  # speed: speed of the tool [m/s]
         0.3,  # acceleration: acceleration of the tool [m/s^2]
@@ -63,9 +63,9 @@ def lower_piece(pos):
             robot_position[0] / 1000,  # x
             robot_position[1] / 1000,  # y
             BOARD_HEIGHT,  # z (height to lift piece)
-            0.546,  # rx
-            3.062,  # ry
-            0.045,  # rz
+            0.546,  # rx (x rotation of TCP in radians)
+            3.062,  # ry (y rotation of TCP in radians)
+            0.045,  # rz (z rotation of TCP in radians)
         ],
         0.5,  # speed: speed of the tool [m/s]
         0.3,  # acceleration: acceleration of the tool [m/s^2]
@@ -73,10 +73,10 @@ def lower_piece(pos):
 
 
 # Opening JSON file
-f = open("setup.json")
+f = open("setup.json", encoding="utf-8")
 data = json.load(f)
 
-move = input("Enter move: ")
+move = input("Enter move (SAN format): ")
 move_from = move[:2]
 move_to = move[-2:]
 from_position = data[move_from]
