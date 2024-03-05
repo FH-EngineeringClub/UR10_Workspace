612e5011dc462831052b6fe70a278ffe98303f1a
Nic Glazkov <nic@glazkov.com>
2024-03-04 22:59:49 -0800

    UR10 stockfish prototype

--- UR10_working_examples/UR10_stockfish.py
+++ UR10_working_examples/UR10_stockfish.py
@@ -1,3 +1,7 @@
+"""
+import python-chess and stockfish to play chess
+"""
+
 from time import sleep
 import socket
 import json
@@ -5,6 +9,12 @@ import math
 import rtde_io
 import rtde_receive
 import rtde_control
+import platform
+import subprocess
+import chess
+import chess.svg
+from stockfish import Stockfish
+from colorama import Fore
 
 HOSTNAME = "192.168.56.101"  # Replace with the IP address of your Universal Robot
 HOST_PORT = 30002  # The port to send commands to the robot
@@ -23,6 +33,23 @@ LIFT_HEIGHT = 0.40  # height of the lift (in meters)
 TCP_RX = 1.393  # rx (x rotation of TCP in radians)
 TCP_RY = -2.770  # ry (y rotation of TCP in radians)
 TCP_RZ = -0.085  # rz (z rotation of TCP in radians)
+
+osSystem = platform.system()  # Get the OS
+if osSystem == "Darwin" or "Linux":
+    stockfishPath = subprocess.run(
+        ["which", "stockfish"], capture_output=True, text=True, check=True
+    ).stdout.strip("\n")  # noqa: E501
+elif osSystem == "Windows":
+    stockfishPath = input("Please enter the full path to the stockfish executable:")
+else:
+    exit("No binary or executable found for stockfish")
+
+stockfish = Stockfish(path=stockfishPath)
+stockfish.set_depth(20)  # How deep the AI looks
+stockfish.set_skill_level(20)  # Highest rank stockfish
+stockfish.get_parameters()  # Get all the parameters
+
+board = chess.Board()  # Create a new board
 
 
 def translate(x, y):
@@ -109,15 +136,20 @@ OUTPUT_0 = "sec myProg():\n\
 end\n\
 myProg()\n"
 
-# Opening JSON file
+
+def display_board():
+    """
+    Display the chess board by writing it to a SVG file.
+    """
+    with open(
+        "chess.svg", "w", encoding="utf-8"
+    ) as f:  # Open a file to write to with explicit encoding
+        f.write(chess.svg.board(board))  # Write the board to the file
+
+
+# Opening UR10 head positions JSON file
 f = open("setup.json", encoding="utf-8")
 data = json.load(f)
-
-move = input("Enter move (SAN format): ")  # e.g. "e2e4" or "e7e5"
-move_from = move[:2]  # from square
-move_to = move[-2:]  # to square
-from_position = data[move_from]
-to_position = data[move_to]
 
 
 def direct_move_piece(from_pos, to_pos, board_height, lift_height):
@@ -142,4 +174,65 @@ def direct_move_piece(from_pos, to_pos, board_height, lift_height):
         print("Piece moved successfully!")
 
 
-direct_move_piece(from_position, to_position, BOARD_HEIGHT, LIFT_HEIGHT)
+display_board()  # Display the board
+
+while not board.is_game_over():
+    print(Fore.WHITE + "Legal moves:", [move.uci() for move in board.legal_moves])
+    inputmove = input(
+        "Input move from the following legal moves (SAN format):"
+    )  # Get the move from the user
+
+    user_confirmation = input("Are you sure you want to make this move? (y/n)")
+    if user_confirmation != "y":
+        print(
+            Fore.RED + "Move not confirmed, please try again"
+        )  # If the user doesn't confirm the move, ask for a new move
+        continue  # Skip the rest of the loop and start from the beginning
+
+    valid_move = (
+        chess.Move.from_uci(inputmove) in board.legal_moves
+    )  # Check if the move is valid
+
+    if valid_move is True:
+        board.push_san(inputmove)  # Push the move to the board
+
+        display_board()  # Display the board
+
+        stockfish.set_fen_position(board.fen())  # Set the position of the board
+        bestMove = stockfish.get_top_moves(1)  # Get the best move
+        target_square = chess.Move.from_uci(bestMove[0]["Move"]).to_square
+
+        if board.piece_at(target_square) is None:
+            print(Fore.CYAN + "Space not occupied")
+
+            move = bestMove  # e.g. "e2e4" or "e7e5"
+            move_from = move[:2]  # from square
+            move_to = move[-2:]  # to square
+            from_position = data[move_from]
+            to_position = data[move_to]
+            direct_move_piece(from_position, to_position, BOARD_HEIGHT, LIFT_HEIGHT)
+
+        else:
+            print(
+                Fore.CYAN + "Space occupied by",
+                board.piece_at(target_square),
+                "removing piece...",
+            )
+
+            move = bestMove  # e.g. "e2e4" or "e7e5"
+            move_from = move[:2]  # from square
+            move_to = move[-2:]  # to square
+            from_position = data[move_from]
+            to_position = "ex"
+            direct_move_piece(from_position, to_position, BOARD_HEIGHT, LIFT_HEIGHT)
+
+        print(
+            Fore.GREEN + "Stockfish moves:", board.push_san(bestMove[0]["Move"])
+        )  # Push the best move to the board
+
+        display_board()  # Display the board
+
+    else:
+        print(Fore.RED + "Not a legal move, Please try again")
+
+print(board.outcome())  # Print the winner of the game
