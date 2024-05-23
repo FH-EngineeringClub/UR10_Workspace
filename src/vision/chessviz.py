8f726fdb76696df7a6e0129e8275f782efd744ac
Alex Hovakimyan <hovak101@gmail.com>
2024-05-22 22:32:48 -0700

    added square localization on chessboard from detected corners

--- src/vision/chessviz.py
+++ src/vision/chessviz.py
@@ -4,33 +4,50 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 class ChessViz:
-    def __init__(self, big_crop, small_crop, skim_percent=0.1, 
-                 variance=5, occupied_threshold = 50, cam_index=1):
-        self.big_crop = big_crop                        # [[y value, x value], width, height]
-        self.small_crop = small_crop                    # [[y value, x value], width, height]
-        self.skim_percent = skim_percent
-        self.variance = variance
-        self.occupied_threshold = occupied_threshold
+    ARUCO_DICT = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+    CHESS_DICT = {
+        0: 'p', 
+        1: 'B', 
+        2: 'N', 
+        3: 'k', 
+        4: 'Q', 
+        5: 'P', 
+        6: 'b', 
+        7: 'r', 
+        8: 'K', 
+        9: 'q', 
+        10: 'n', 
+        11: 'R'
+    }
+    
+    def __init__(self, big_crop, small_crop, cam_index=1):
+        self.big_crop = big_crop                        # [[y value, x value], sidelength]
+        self.small_crop = small_crop                    # [[y value, x value], sidelength]
         self.cam_index = cam_index
-
+        
+        # find origin of small relative to big
+        self.y_origin = self.small_crop[0][0] - self.big_crop[0][0]
+        self.x_origin = self.small_crop[0][1] - self.big_crop[0][1]
+        if self.x_origin < 0 or self.y_origin < 0:
+            raise Exception("big crop's origin must be larger in both x and y")
+        
         # takes picture and determines resolution width and height
         # from picture dimensions
         cam = cv2.VideoCapture(cam_index, cv2.CAP_DSHOW)
         ret, image = cam.read() 
         self.resolution_width = image.shape[1]
         self.resolution_height = image.shape[0] 
-
+        
     # For tkinter gui
-    def __update_crop_params(self, crop_params, x_var, y_var, width_var, height_var):
+    def __update_crop_params(self, crop_params, x_var, y_var, sidelength_var):
         crop_params[0][1] = int(x_var.get())
         crop_params[0][0] = int(y_var.get())
-        crop_params[1] = int(width_var.get())
-        crop_params[2] = int(height_var.get())
+        crop_params[1] = int(sidelength_var.get())
 
     # For tkinter gui
     def __show_frame(self, crop_params, cap, lmain):
         _, frame = cap.read()
-        cropped_frame = frame[crop_params[0][0]:(crop_params[0][0] + crop_params[2]), 
+        cropped_frame = frame[crop_params[0][0]:(crop_params[0][0] + crop_params[1]), 
                             crop_params[0][1]:(crop_params[0][1] + crop_params[1])]
         cv_img = cv2.cvtColor(cropped_frame, cv2.COLOR_BGR2RGB)
         img = Image.fromarray(cv_img)
@@ -73,32 +90,23 @@ class ChessViz:
         y_slider.pack(side='left')
         y_entry.pack(side='left')
 
-        frame_width = Frame(root)
-        frame_width.pack(fill='x', expand=True)
-        width_var = StringVar()
-        width_slider = Scale(frame_width, from_=1, to=self.resolution_width - 1, 
-                             orient=HORIZONTAL, variable=width_var, 
-                             label="Crop Width")
-        width_entry = Entry(frame_width, textvariable=width_var)
-        width_slider.pack(side='left')
-        width_entry.pack(side='left')
-
-        frame_height = Frame(root)
-        frame_height.pack(fill='x', expand=True)
-        height_var = StringVar()
-        height_slider = Scale(root, from_=1, to=self.resolution_height - 1, 
-                              orient=HORIZONTAL, variable=height_var, label="Crop Height")
-        height_entry = Entry(root, textvariable=height_var)
-        height_slider.pack(side='left')
-        height_entry.pack(side='left')
+        frame_sidelength = Frame(root)
+        frame_sidelength.pack(fill='x', expand=True)
+        sidelength_var = StringVar()
+        sidelength_var_slider = Scale(frame_sidelength, from_=1, to=self.resolution_width - 1, 
+                             orient=HORIZONTAL, variable=sidelength_var, 
+                             label="Crop Sidelength")
+        sidelength_var_entry = Entry(frame_sidelength, textvariable=sidelength_var)
+        sidelength_var_slider.pack(side='left')
+        sidelength_var_entry.pack(side='left')
         
         # Button to update cropping parameters
         button = Button(root, text="Update Crop", 
                         command=lambda: self.__update_crop_params(
-                            crop_params, x_var, y_var, width_var, height_var))
+                            crop_params, x_var, y_var, sidelength_var))
         button.pack()
         root.bind('<Return>', lambda event: self.__update_crop_params(
-            crop_params, x_var, y_var, width_var, height_var))
+            crop_params, x_var, y_var, sidelength_var))
 
         self.__show_frame(crop_params, cap, lmain)  # Start showing the frame
         root.mainloop()  # Start the GUI
@@ -115,107 +123,53 @@ class ChessViz:
         return image
     
     def get_crop(self, image, crop_params):
-        return image[crop_params[0][0]:(crop_params[0][0] + crop_params[2]), 
+        return image[crop_params[0][0]:(crop_params[0][0] + crop_params[1]), 
                         crop_params[0][1]:(crop_params[0][1] + crop_params[1])]
 
     # TODO: Uses canny edge detection to set crop_width, crop_height,
     # and crop_origin automatically
     def find_chessboard():
         pass
-
-    # Takes cropped chessboard and evenly splits crop into 8 by 8 squares
-    # input: numpy array of shape (crop height, crop width)
-    # output: numpy array of shape (8, 8, square height, square width)
-    def create_squares_array(self, image):
-        self.square_width = round(image.shape[1] / 8)
-        self.square_height = round(image.shape[0] / 8)
-        squares_array = np.empty((8, 8), dtype=object)
-
-        # Crop out squares from chessboard and create 8 by 8 array of squares
-        for i in range(8):
-            for j in range(8):
-                squares_array[i][j] = image[i * self.square_height:(i + 1) * self.square_height,
-                                            j * self.square_width:(j + 1) * self.square_width,] 
-                
-        return squares_array
-
-    # Since crop is not perfect, this function shaves off some percent of 
-    # pixels off each squares' borders. The percent is defined in the global 
-    # variable "skim_percent"
-    # input: numpy array of shape (8, 8, square height, square width)
-    # output: numpy array of shape (8, 8, square height - square height * skim percent,
-    # square width - square width * skim percent)
-    def skim_squares_array(self, squares_array):
-        print("square_width: ", self.square_width)
-        print("square_hegiht: ", self.square_height)
-        for i in range(8):
-            for j in range(8): 
-                squares_array[i][j] = squares_array[i][j][round(self.skim_percent*self.square_height):
-                                                          round(-self.skim_percent*self.square_height), 
-                                                          round(self.skim_percent*self.square_width):
-                                                          round(-self.skim_percent*self.square_width)]
-
-    # Exists for sole purpose of numpy vectorization. Uses standard deviation of 
-    # greyscale values in order to measure contrast within an image. A higher deviation
-    # represents higher contrast and thus an increased likehood of a piece being in that 
-    # square. 
-    def __calculate_contrast(self, image):
-        return np.round(image.std())
-
-    # Takes squares array, finds standard deviation for each square, and applies a threshold
-    # and variance(error) value to determine whether a square is filled or not. A value
-    # of 1 indicates it is filled. A value of 0 indicates it is not.  
-    # input: numpy array of shape (8, 8, square height - square height * skim percent,
-    # square width - square width * skim percent)
-    # output: numpy array of shape (8, 8, 1)
-    def find_occupied_from_threshold(self, squares_array):
-        # Vectorize function in order to make it numpy iterable
-        vectorized_contrast = np.vectorize(self.__calculate_contrast)
-        contrast_values = vectorized_contrast(squares_array)
-
-        # if a contrast value is in a certain threshold +- some error, 
-        # then the space is occupied
-        mask = ((contrast_values > self.occupied_threshold - self.variance) & 
-                        (contrast_values < self.occupied_threshold + self.variance))
-
-        occupied_array = np.zeros((8, 8))
-        occupied_array[mask] = 1
-        return occupied_array
-
-    # Simply prints a numpy array containing the standard deviation value for each 
-    # sqaure. 
-    # input: numpy array of shape (8, 8, square height - square height * skim percent,
-    # square width - square width * skim percent)
-    # output: numpy array of shape (8, 8, 1)
-    def find_threshold_values(self, squares_array):
-        # Vectorize function in order to make it numpy iterable
-        vectorized_func = np.vectorize(self.__calculate_contrast)
-        contrast_values = vectorized_func(squares_array)
-        print(contrast_values)
         
-    def get_centers(self, corners_array):
-        center_array = np.empty((np.size(corners_array, 0), 2))
+    def get_center(self, corners):
+        corners = corners.reshape((4, 2))
+        (top_left, top_right, bottom_right, bottom_left) = corners
         
-        for i in range(np.size(corners_array, 0)):
-            corners = corners_array[i].reshape((4, 2))
-            (top_left, top_right, bottom_right, bottom_left) = corners
-            
-            # Convert the (x,y) coordinate pairs to integers
-            top_right = (int(top_right[0]), int(top_right[1]))
-            bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
-            bottom_left = (int(bottom_left[0]), int(bottom_left[1]))
-            top_left = (int(top_left[0]), int(top_left[1]))
-        
-            # Calculate and draw the center of the ArUco marker
-            center_y = int((top_left[0] + bottom_right[0]) / 2.0)
-            center_x = int((top_left[1] + bottom_right[1]) / 2.0)
-            center_array[i] = (center_y, center_x)
-            
-        return center_array
+        # Convert the (x,y) coordinate pairs to integers
+        top_right = (int(top_right[0]), int(top_right[1]))
+        bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
+        bottom_left = (int(bottom_left[0]), int(bottom_left[1]))
+        top_left = (int(top_left[0]), int(top_left[1]))
     
-    def get_chess_array(self, centers, ids, chess_image):
-        pass
+        # Calculate and draw the center of the ArUco marker
+        center_y = int((top_left[0] + bottom_right[0]) / 2.0)
+        center_x = int((top_left[1] + bottom_right[1]) / 2.0)
+        return (center_y, center_x)
     
+    def modified_step(self, value, div, min, max):
+        value = value // div
+        if value > max:
+            value = max
+        if value < min:
+            value = min
+            
+        return value
+            
+    def get_chess_piece(self, center_y, center_x, id, chess_array):
+        # subtract relative origin of small from centers
+        center_y = center_y - self.y_origin
+        center_x = center_x - self.x_origin
+        
+        # divide center y and x by square width to acquire
+        # square coordinates
+        square_len = self.small_crop[1] // 8
+        center_y = self.modified_step(center_y, square_len, 0, 7)
+        center_x = self.modified_step(center_x, square_len, 0, 7)
+        
+        # convert square coordinates to chess array 
+        chess_array[center_y, center_x] = self.CHESS_DICT[id[0]]
+        return chess_array
+
     def resize_image(self, image, factor):
         # Get the original image dimensions
         h, w = image.shape[:2]
@@ -225,20 +179,20 @@ class ChessViz:
         cap = cv2.VideoCapture(self.cam_index, cv2.CAP_DSHOW)
         total = 0
         detected = 0
-        aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
 
         while(cap.isOpened()):
+            chess_array = np.full((8, 8), '', dtype='U1')
             ret, frame = cap.read()
-            frame = self.get_chessboard(frame) 
+            frame = self.get_crop(frame, self.big_crop) 
             frame = self.resize_image(frame, 1)
             total += 1
             # Detect ArUco markers in the video frame
-            (corners, ids, rejected) = cv2.aruco.detectMarkers(frame, aruco_dict)
-            # Check that at least one ArUco marker was detected
+            (corners, ids, rejected) = cv2.aruco.detectMarkers(frame, self.ARUCO_DICT)
+            if len(corners) == 32:
+                detected += 1
+            
+            print(ids)
             if len(corners) > 0:
-                if len(corners) == 32:
-                    detected += 1
-                
                 # Flatten the ArUco IDs list
                 # ids = ids.flatten()
                 # # Loop over the detected ArUco corners
@@ -261,9 +215,9 @@ class ChessViz:
                     cv2.line(frame, bottom_left, top_left, (0, 255, 0), 2)
                     
                     # Calculate and draw the center of the ArUco marker
-                    center_x = int((top_left[0] + bottom_right[0]) / 2.0)
-                    center_y = int((top_left[1] + bottom_right[1]) / 2.0)
-                    cv2.circle(frame, (center_x, center_y), 4, (0, 0, 255), -1)
+                    center_y = int((top_left[0] + bottom_right[0]) / 2.0)
+                    center_x = int((top_left[1] + bottom_right[1]) / 2.0)
+                    cv2.circle(frame, (center_y, center_x), 4, (0, 0, 255), -1)
                     
                     # Draw the ArUco marker ID on the video frame
                     # The ID is always located at the top_left of the ArUco marker
@@ -271,10 +225,11 @@ class ChessViz:
                     (top_left[0], top_left[1] - 15),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     0.5, (0, 255, 0), 2)
-        
-                # Display the resulting frame
-            cv2.imshow('frame',frame)
-                
+                    self.get_chess_piece(center_y, center_x, marker_id, chess_array)
+                    
+            # Display the resulting frame
+            cv2.imshow('frame', frame)
+            print(chess_array)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
             
--- src/vision/test_aruco_detection.py
+++ src/vision/test_aruco_detection.py
@@ -1,5 +1,5 @@
 import cv2
 from chessviz import ChessViz
 
-chessviz = ChessViz([0, 0], 300, 300, cam_index=0)
+chessviz = ChessViz([[0, 0], 400], [[25, 25], 300], cam_index=0)
 chessviz.test_aruco_detection()
